bef971fb362c229a4beeb1c308e92a6188c7093a
unknown <eduardo.johnson.guerrero@estudiante.ipss.cl>
2025-10-27 20:13:26 -0300

    Proyecto spring

---
+++ prueba-sprint/src/main/java/com/example/prueba_sprint/PruebaSprintApplication.java
@@ -0,0 +1,13 @@
+package com.example.prueba_sprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaSprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaSprintApplication.class, args);
+	}
+
+}
---
+++ prueba-sprint/src/test/java/com/example/prueba_sprint/PruebaSprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prueba_sprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaSprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
